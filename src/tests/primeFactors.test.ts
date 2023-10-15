974297b487ea57a8596d8aabe5c89e53881296ed
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2023-10-15 14:07:36 +0100

    green: finds the prime composition of the given number

--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -10,10 +10,13 @@
 */
 
 function getPrimeFactorsFor(number: number) {
-    const factor = 2;
+    let factor = 2;
+    if (number % factor !== 0) {
+      factor = 3;
+    }
     const factors = [factor];
     const remainder = number / factor;
-    if(remainder > 1) {
+    if (remainder > 1) {
       return factors.concat(getPrimeFactorsFor(remainder));
     }
     return factors;
