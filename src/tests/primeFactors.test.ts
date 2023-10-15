b58372bdd93a06af655b8e00b1f5ec8c6f2d3ad7
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2023-10-15 13:54:57 +0100

    red: finds the prime composition of the given number

--- src/tests/primeFactors.test.ts
+++ src/tests/primeFactors.test.ts
@@ -1,3 +1,20 @@
-describe("The Prime Factorizer", ()=>{
-   
-});
+/*
+2 -> [2]
+2 * 2 -> [2,2]
+2 * 2 * 2 -> [2,2,2]
+3 -> [3]
+3 * 3 -> [3,3]
+3 * 2 -> [2,3]
+5 * 5 -> [5,5]
+5 * 7 * 11 * 3 -> [3,5,7,11]
+*/
+
+function getPrimeFactorsFor(number: number) {
+    return null;
+  }
+  
+  describe('The prime factors', () => {
+    it('finds the prime composition of the given number', () => {
+      expect(getPrimeFactorsFor(2)).toEqual([2]);
+    });
+  });
