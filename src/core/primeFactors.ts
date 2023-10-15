59ebc37a1278163e6409f869c2373d6edb181dd2
Mónica Ilenia Tardón Vera <monitvera@gmail.com>
2023-10-15 14:29:51 +0100

    refactor: reorganize primeFactors

--- src/core/primeFactors.ts
+++ src/core/primeFactors.ts
@@ -1,18 +1,23 @@
-function findSmallestPrime(number: number) {
-    if (number === 1) return 1;
-    
-    let factor = 2;
-
-    while (number % factor !== 0) {
-      ++factor;
-    }
-
-    return factor;
+export function getPrimeFactorsFor(number: number) {
+    checkForPositiveNumber(number);
+    return primeFactors(number);
 }
   
-export function getPrimeFactorsFor(number: number) {
+function checkForPositiveNumber(number: number) {
     if (number < 1) throw new Error('Only positive numbers are allowed');
+}
+  
+function primeFactors(number: number) {
     const prime = findSmallestPrime(number);
     const remainder = number / prime;
     return remainder <= 1 ? [prime] : [prime].concat(getPrimeFactorsFor(remainder));
+}
+  
+function findSmallestPrime(number: number) {
+    if (number === 1) return 1;
+    let factor = 2;
+    while (number % factor !== 0) {
+      ++factor;
+    }
+    return factor;
 }
